77758ae88777efa1a2b6cb0c230208cd17037110
Mohammad Allahham <109595065+mohallahham@users.noreply.github.com>
2025-05-31 14:52:58 +0300

    9-5. Protected Blog

--- blogs/models.py
+++ blogs/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 
 
 class Blog(models.Model):
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     name = models.CharField(max_length=200)
     description = models.TextField()
 
@@ -14,6 +16,7 @@ class Blog(models.Model):
 
 class Post(models.Model):
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="posts")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
--- blogs/views.py
+++ blogs/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseForbidden
+
 
 from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
@@ -12,7 +14,7 @@ from .forms import BlogForm, PostForm, UserRegisterForm
 
 
 def home(request):
-    posts = Post.objects.all().order_by("-created_at")
+    posts = Post.objects.all().order_by("-created_at")  # 👈 Show ALL posts
     return render(request, "blogs/home.html", {"posts": posts})
 
 
@@ -21,12 +23,12 @@ def create_blog(request):
     if request.method == "POST":
         form = BlogForm(request.POST)
         if form.is_valid():
-            form.save()
+            blog = form.save(commit=False)
+            blog.owner = request.user
+            blog.save()
             return redirect("home")
-
     else:
         form = BlogForm()
-
     return render(request, "blogs/create_blog.html", {"form": form})
 
 
@@ -35,7 +37,9 @@ def create_post(request):
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            form.save()
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
             return redirect("home")
     else:
         form = PostForm()
@@ -45,6 +49,10 @@ def create_post(request):
 @login_required
 def edit_post(request, pk):
     post = get_object_or_404(Post, pk=pk)
+
+    if post.author != request.user:  # 👈 Check the Post author now
+        return HttpResponseForbidden("You are not allowed to edit this post.")
+
     if request.method == "POST":
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
